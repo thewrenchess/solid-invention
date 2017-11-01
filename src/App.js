8e710f0afd0529c16bb7f16f37fb51bc1f5ce08f
thewrenchess <ydxu2147483648>
2017-11-01 16:24:45 -0700

    MyRead created, files cleaned up, connected to bootstrap css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  	render() {
+    	return (
+      		<div className='container pt-3'>
+      		<h1 className='text-center'>Hello World!</h1>
+      	</div>
+    	);
+  	}
+}
+
+export default App;
